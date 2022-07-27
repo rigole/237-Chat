9ea14c51351922c6e8dd0fc4fe7b6576b58b41a7
rigole <foplacide@gmail.com>
2022-07-27 16:12:18 +0100

    setting routes and welcome component 2

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,20 +3,22 @@ import { Box } from '@mui/material'
 import SideBar from "../components/SideBar"
 import Welcome from "../components/Welcome";
 import { Routes, Route } from "react-router-dom"
-
+import ChatScreen from "../components/ChatScreen";
 
 const AllRoutes = () => {
     return (
         <Routes>
             <Route path="" element={<Welcome/>}/>
-            <Route path="/:id/:name" element={<Welcome/>}/>
+            <Route path="/:id/:name" element={<ChatScreen/>}/>
         </Routes>
     )
 }
 
 const  HomePage =  () =>{
     return (
-        <Box>
+        <Box
+        display="flex"
+        >
             <SideBar/>
             <AllRoutes/>
         </Box>
