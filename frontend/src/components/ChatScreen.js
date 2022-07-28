87fb5f1d8daedf7baf8384a0f5f36b96ddc1568a
rigole <foplacide@gmail.com>
2022-07-28 21:23:59 +0100

    ChatScreen editing 1

--- frontend/src/components/ChatScreen.js
+++ frontend/src/components/ChatScreen.js
@@ -1,10 +1,31 @@
 import React from 'react'
+
+import { Box, AppBar, Toolbar, Typography,Avatar } from "@mui/material"
 import {useParams} from "react-router-dom";
 
 const ChatScreen = () => {
     const {id, name} = useParams()
+
+
+
+
     return (
-        <div>{id} {name}</div>
+
+        <Box
+        flexGrow={1}
+        >
+            <AppBar position="static"
+            sx={{backgroundColor:"white", boxShadow:0}}>
+                <Toolbar>
+                    <Avatar
+                        src={`https://avatars.dicebear.com/api/initials/${name}.svg`}
+                        sx={{width:"32px", height:"32px"}}
+                    />
+                    <Typography variant="h6" color="black">{name}</Typography>
+                </Toolbar>
+            </AppBar>
+
+        </Box>
     )
 }
 
